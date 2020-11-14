958c91c77c8a67e40f3b16cbc03d2e5eb31b9b7d
shirui10 <240816440@qq.com>
2020-11-14 18:25:16 +0800

    demo

---
+++ XIANGQI for Mac/Display/View.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using Model;
+using Control;
+
+namespace View
+{
+    public class ProgramView
+    {
+        static void Main(string[] args)
+        {
+            ProCon con = new ProCon();
+            ProMod mod = new ProMod();
+            ProgramView view = new ProgramView();
+            Chess[,] Matrix = mod.Resetground();
+            Chess[,] road = mod.Setroad();
+            bool result = true;
+            bool turn;
+            int player = 0;
+
+            while (result == true)
+            {
+                string[,] Board = mod.Piece(Matrix);
+                view.Displaying(Matrix, road);
+                view.Start(player);
+
+                try
+                {
+                    Console.Write("============================================\n");
+                    Console.Write("                   X = ");
+                    int chozenX = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("                   Y = ");
+                    int chozenY1 = Convert.ToInt32(Console.ReadLine());
+                    int chozenY = chozenY1 * 2;
+                    int checkpiece = con.Checkpiece(chozenX * 2, chozenY, Matrix);
+
+                    if (checkpiece == 2)//检测是否有棋子
+                    {
+                        road = con.Road(chozenX * 2, chozenY, Matrix);
+                        view.Displaying(Matrix, road);
+                        road = mod.Setroad();
+                        view.Check(checkpiece, Board, chozenX, chozenY);
+                        Console.Write("                   X = ");
+                        int X = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("                   Y = ");
+                        int Y = Convert.ToInt32(Console.ReadLine());
+                        turn = con.Turn(X * 2, Y * 2, chozenX * 2, chozenY, Matrix);
+                        player = view.Move(turn, player);
+                        result = con.Result(Matrix);
+                    }
+                    else if (checkpiece == 0)
+                    {
+                        view.Check(checkpiece, Board, chozenX, chozenY);
+                    }
+                    else
+                    {
+                        view.Check(checkpiece, Board, chozenX, chozenY);
+                    }
+                }
+
+                catch (Exception)
+                {
+                    view.Wrong();
+                }
+            }
+
+            view.Displaying(Matrix, road);
+            view.Win(player);
+            Console.ReadKey();
+        }
+
+
+        public void Start(int start)
+        {
+            if (start % 2 == 0)
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("\n");
+                Console.Write("                   Hello!                   \n");
+                Console.Write("                    RED                     \n");
+                Console.Write("       Please set your position(X & Y):     \n");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("\n");
+                Console.Write("                  Hello!                     \n");
+                Console.Write("                  BLACK                      \n");
+                Console.Write("       Please set your position(X & Y):      \n");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+        }
+
+
+        public int Move(bool turn, int player)
+        {
+            if (turn == false)
+            {
+                Console.Write(" You cannot move there and set again please! ");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.Write("               Move successly!               ");
+                Console.ReadLine();
+                player++;
+            }
+
+            return player;
+        }
+
+
+        public void Check(int checkpiece, string[,] Board, int chozenX, int chozenY)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            if (checkpiece == 2)
+            {
+                Console.Write("\n        You have the piece: " + Board[chozenX * 2, chozenY] + "(" + chozenX + "," + chozenY / 2 + ").\n");
+                Console.Write("  Please set your position of moving(X & Y):\n");
+                Console.Write("============================================\n");
+            }
+            else if (checkpiece == 0)
+            {
+                Console.Write("              There is no piece.            \n");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.Write("             You cannot choose it.          \n");
+                Console.ReadLine();
+            }
+        }
+
+
+        public void Wrong()
+        {
+            Console.Write("  You cannot set this and set again please! ");
+            Console.ReadLine();
+        }
+
+
+        public void Win(int player)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            if (player % 2 == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("\n============================================\n");
+                Console.Write("                  RED WIN!!                   ");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("\n============================================\n");
+                Console.Write("                 BLACK WIN!!                  ");
+            }
+        }
+
+
+        public void Displaying(Chess[,] Matrix, Chess[,] road)
+        {
+            ProMod Mod = new ProMod();
+            string[,] Board = Mod.Piece(Matrix);
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write(" X\\Y ");
+
+            for (int j = 0; j <= 17; j++)
+            {
+                if (j == 0)
+                {
+                    Console.Write(j + "   ");
+                }
+                else if (j == 16)
+                {
+                    Console.Write(j / 2 + "      ");
+                }
+                else if (j % 2 == 0 && j > 0)
+                {
+                    Console.Write(j / 2 + "   ");
+                }
+            }
+
+            Console.Write("\n");
+
+            for (int i = 0; i <= 18; i++)//把棋盘和辅助坐标打印出来
+            {
+                //Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                if (i % 2 == 0)
+                {
+                    Console.Write("  " + i / 2 + "  ");
+                }
+                else
+                {
+                    Console.Write("     ");
+                }
+                //Console.BackgroundColor = ConsoleColor.Yellow;
+
+                for (int j = 0; j < 17; j++)
+                {
+                    if (Matrix[i, j].side == Chess.Player.blue)
+                    {
+                        if (road[i, j].path == Chess.Piecepath.yes)
+                        {
+                            Colorgreen(Board, i, j);
+                            road[i, j].path = Chess.Piecepath.not;
+                        }
+                        else
+                        {
+                            Coloryellow(Matrix, Board, i, j);
+                        }
+                    }
+                    else if (Matrix[i, j].side == Chess.Player.red)
+                    {
+                        if (road[i, j].path == Chess.Piecepath.yes)
+                        {
+                            Colorgreen(Board, i, j);
+                            road[i, j].path = Chess.Piecepath.not;
+                        }
+                        else
+                        {
+                            Coloryellow(Matrix, Board, i, j);
+                        }
+                    }
+                    else
+                    {
+                        if (road[i, j].path == Chess.Piecepath.yes)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Blue;
+                            Console.Write(Board[i, j]);
+                            Console.BackgroundColor = ConsoleColor.Yellow;
+                            road[i, j].path = Chess.Piecepath.not;
+                        }
+                        else
+                        {
+                            Console.Write(Board[i, j]);
+                        }
+                    }
+                }
+               // Console.BackgroundColor = ConsoleColor.Black;
+                if (i % 2 == 0)
+                {
+                    Console.Write("  " + i / 2 + "  ");
+                }
+                else
+                {
+                    Console.Write("     ");
+                }
+                Console.Write("\n");
+            }
+
+            //Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("     ");
+
+            for (int j = 0; j <= 17; j++)
+            {
+                if (j == 0)
+                {
+                    Console.Write(j + "   ");
+                }
+                else if (j == 16)
+                {
+                    Console.Write(j / 2 + "      ");
+                }
+                else if (j % 2 == 0 && j > 0)
+                {
+                    Console.Write(j / 2 + "   ");
+                }
+            }
+        }
+
+
+        public void Coloryellow(Chess[,] Matrix, string[,] Board, int i, int j)
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+
+            if (Matrix[i, j].side == Chess.Player.red)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if (Matrix[i, j].side == Chess.Player.blue)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+
+            Console.Write(Board[i, j]);
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            //Console.BackgroundColor = ConsoleColor.Yellow;
+        }
+
+
+        public void Colorgreen(string[,] Board, int i, int j)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write(Board[i, j]);
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+        }
+
+    }
+}
---
+++ XIANGQI for Windows/Control/Control.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using Model;
+
+
+namespace Control
+{
+    public class ProCon
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+        public int turn = 0;        //回合数从0开始
+
+
+        public bool SwitchPlayer(int X, int Y, int chozenX, int chozenY, Chess[,] Matrix)       //选棋子，让其在一方回合时不能选择对方棋子
+        {
+            if (turn == 0)
+            {
+                if (Matrix[chozenX, chozenY].side != Chess.Player.red)
+                {
+                    return false;
+                }
+                else
+                {
+                    bool check = MovePiece(X, Y, chozenX, chozenY, Matrix);
+                    if (check == true)
+                    {
+                        turn = 1;
+                        return true;
+                    }
+                    else return false;
+                }
+            }
+            else if (turn == 1)
+            {
+                if (Matrix[chozenX, chozenY].side != Chess.Player.blue)
+                {
+                    return false;
+                }
+                else
+                {
+                    bool check = MovePiece(X, Y, chozenX, chozenY, Matrix);
+                    if (check == true)
+                    {
+                        turn = 0;
+                        return true;
+                    }
+                    else return false;
+                }
+            }
+            else return false;
+        }
+
+
+        public bool Result(Chess[,] Matrix)       //判断结果
+        {
+            int n = 0;
+            bool result = true;
+
+            for (int i = 0; i < 19; i++)
+            {
+                for (int j = 0; j < 17; j++)
+                {
+                    if (Matrix[i, j].type == Chess.Piecetype.jiang)
+                    {
+                        n++;
+                    }
+                }
+            }
+
+            if (n == 2)
+            {
+                return result;
+            }
+            else
+            {
+                result = false;
+                return result;
+            }
+        }
+
+
+        public bool MovePiece(int X, int Y, int chozenX, int chozenY, Chess[,] Matrix)       //定义每种棋子的移动方式
+        {
+            Rook che = new Rook();
+            Horse ma = new Horse();
+            Elephant xiang = new Elephant();
+            Advisor shi = new Advisor();
+            General jiang = new General();
+            Cannon pao = new Cannon();
+            Soldier bing = new Soldier();
+            bool re;
+
+            switch (Matrix[chozenX, chozenY].type)
+            {
+                case Chess.Piecetype.che:
+                    re = che.Che(X, Y, chozenX, chozenY, Matrix);
+                    return re;
+                case Chess.Piecetype.ma:
+                    re = ma.Ma(X, Y, chozenX, chozenY, Matrix);
+                    return re;
+                case Chess.Piecetype.xiang:
+                    re = xiang.Xiang(X, Y, chozenX, chozenY, Matrix);
+                    return re;
+                case Chess.Piecetype.shi:
+                    re = shi.Shi(X, Y, chozenX, chozenY, Matrix);
+                    return re;
+                case Chess.Piecetype.jiang:
+                    re = jiang.Jiang(X, Y, chozenX, chozenY, Matrix);
+                    return re;
+                case Chess.Piecetype.pao:
+                    re = pao.Pao(X, Y, chozenX, chozenY, Matrix);
+                    return re;
+                case Chess.Piecetype.bing:
+                    re = bing.Bing(X, Y, chozenX, chozenY, Matrix);
+                    return re;
+            }
+
+            return false;
+        }
+
+
+        public Chess[,] Road(int chozenX, int chozenY, Chess[,] Matrix)      // 使棋子显示可行路径时，并使移动前的棋子不移动
+        {
+            ProMod mod = new ProMod();
+            Chess[,] road = mod.SetRoad();
+            Chess[,] trans = new Chess[19, 17];
+            bool cr;
+
+            for (int i = 0; i < 19; i++)
+            {
+                for (int j = 0; j < 17; j++)
+                {
+                    trans[i, j] = new Chess();
+                }
+            }
+
+            for (int i = 0; i < 19; i++)
+            {
+                for (int j = 0; j < 17; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        trans[i, j].side = Matrix[i, j].side;
+                        trans[i, j].type = Matrix[i, j].type;
+                        trans[chozenX, chozenY].side = Matrix[chozenX, chozenY].side;
+                        trans[chozenX, chozenY].type = Matrix[chozenX, chozenY].type;
+                        cr = MovePiece(i, j, chozenX, chozenY, Matrix);
+
+                        if (cr == true)
+                        {
+                            road[i, j].path = Chess.Piecepath.yes;
+                        }
+
+                        Matrix[i, j].side = trans[i, j].side;
+                        Matrix[i, j].type = trans[i, j].type;
+                        Matrix[chozenX, chozenY].side = trans[chozenX, chozenY].side;
+                        Matrix[chozenX, chozenY].type = trans[chozenX, chozenY].type;
+                    }
+                }
+            }
+
+            return road;
+        }
+
+
+        public int CheckPiece(int chozenX, int chozenY, Chess[,] Matrix)                //检测选中的是否棋子
+        {
+            if (Matrix[chozenX, chozenY].type == Chess.Piecetype.god)
+            {
+                return 0;
+            }
+            else if (turn == 0)
+            {
+                if (Matrix[chozenX, chozenY].side != Chess.Player.red)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 2;
+                }
+            }
+            else if (turn == 1)
+            {
+                if (Matrix[chozenX, chozenY].side != Chess.Player.blue)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 2;
+                }
+            }
+
+            return 0;
+        }
+
+
+        public void SetMove(int X, int Y, int chozenX, int chozenY, Chess[,] Matrix)       //基本移动方式
+        {
+            Matrix[X, Y].side = Matrix[chozenX, chozenY].side;
+            Matrix[X, Y].type = Matrix[chozenX, chozenY].type;
+            Matrix[chozenX, chozenY].side = Chess.Player.god;
+            Matrix[chozenX, chozenY].type = Chess.Piecetype.god;
+        }
+    }
+}
